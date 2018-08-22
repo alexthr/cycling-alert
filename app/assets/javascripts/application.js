e4cbb9f68f05e95f206e2efa78b939f464a8f793
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-22 12:06:39 +0200

    Add gem bootstrap 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,8 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
 
