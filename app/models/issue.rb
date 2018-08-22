a2b2700667a5e79faefca91a8521fd1b6bf7964c
yannicksanchez <yannick.sanchez.guasch@gmail.com>
2018-08-22 13:30:55 +0200

    few changes as a test after bootstrap 4 implementation

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -9,6 +9,6 @@ class Issue < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  validates :title, :description, :photos, presence: true
+  validates :title, :description, presence: true
 
 end
