079e8cc4bc5d7f0be5775d845dd9d69e8240d362
Alex Thoreux <alex.thoreux@gmail.com>
2018-08-23 16:01:06 +0200

    basic upvote and downvote without ajax

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,13 @@
 class VotesController < ApplicationController
-  def new
-  end
-
   def create
+    @issue = Issue.find(params[:issue_id])
+    @vote = @issue.votes.new(direction: params[:direction])
+    @vote.user = current_user
+    if @vote.save
+      redirect_to issue_path(@issue)
+    else
+      redirect_to issues_path
+    end
   end
 
   def edit
