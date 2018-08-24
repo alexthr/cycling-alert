7a147c025908b4a79f60d5cdf1c0ae936ba4cf21
yannicksanchez <yannick.sanchez.guasch@gmail.com>
2018-08-24 16:44:47 +0200

    updated code for pictures upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,9 +33,16 @@ viaduc.user = alexthr
 acces.user = visant
 
 trou.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
+croisement.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
+piste.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
+eclairage.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
+dangereux.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
+sens_interdit.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
+travaux.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
+viaduc.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
 acces.photo_urls = ["https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef92ab342dceba763781e4dd9f9b396c&auto=format&fit=crop&w=1995&q=80"]
 
-
+puts 'Photos uploaded'
 
 trou.save
 croisement.save
