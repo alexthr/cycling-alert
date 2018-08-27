ca003407892104c3eee6a94c1f711d50006333ff
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-27 16:32:36 +0200

    Modify minor css

--- app/javascript/components/dynamic-navbar-button/dynamic-navbar-button.js
+++ app/javascript/components/dynamic-navbar-button/dynamic-navbar-button.js
@@ -1,5 +1,4 @@
 const btnNavbar = document.getElementById("nav-toggle");
-console.log(btnNavbar)
 if(btnNavbar) {
   btnNavbar.addEventListener("click", (event) => {
     event.currentTarget.classList.toggle("active");
