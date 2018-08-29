c421d68e0c8616517a88b81957faee1d2464dcda
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-29 13:19:26 +0200

    add meta Open Graph meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar, :bio])
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   protected
 
   def after_sign_in_path_for(user)
