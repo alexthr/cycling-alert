b6cd73c92eb1fd533c1c5c160bfa81c7f28c3a01
yannicksanchez <yannick.sanchez.guasch@gmail.com>
2018-08-30 12:08:26 +0200

    if user.admin?, fix_status can be switched to solved by one click

--- app/controllers/fix_reports_controller.rb
+++ app/controllers/fix_reports_controller.rb
@@ -2,7 +2,11 @@ class FixReportsController < ApplicationController
   def create
     @issue = Issue.find(params[:issue_id])
     current_user.fix_reports.create(issue: @issue)
-    @issue.update(fix_status: "resolved") if @issue.fix_reports.count >= 3
+    if current_user.admin?
+      @issue.update(fix_status: 2)
+    elsif current_user.admin? == false && @issue.fix_reports.count >= 3
+      @issue.update(fix_status: 2)
+    end
     redirect_to @issue
   end
 
